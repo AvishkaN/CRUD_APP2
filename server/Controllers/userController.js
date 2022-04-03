9caaf9ee79d21afb6fd94395c385494b06224999
AvishkaN <avishkanirmitha@gmail.com>
2022-04-03 00:38:07 -0800

    log in

--- server/Controllers/userController.js
+++ server/Controllers/userController.js
@@ -26,6 +26,28 @@ export const SignUp=async(req,res)=>{
 };    
 
 
+
+exports.login = catchAsync(async (req, res) => {
+    const { email, password } = req.body;
+  
+    // 1) Check if email and password exist
+    if (!email || !password) {
+        res.status(409).json({message:err.message});  
+
+      return next(new AppError('Please provide email and password!', 400));
+    }
+    // 2) Check if user exists && password is correct
+    const user = await User.findOne({ email }).select('+password');
+  
+    if (!user || !(await user.correctPassword(password, user.password))) {
+      return next(new AppError('Incorrect email or password', 401));
+    }
+  
+    // 3) If everything ok, send token to client
+    createSendToken(user, 200, req, res);
+  });
+
+
 export const editProduct = async (req, res) => {
 
 
