e153f4cedd1d001b1117e369099468fb574d18ac
NIRMITHA <avishkanirmitha@gmail.com>
2022-04-04 07:07:07 +0530

    added enviramental variable

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from "dotenv";
 
 
 import productRouter from "./routes/productRouter.js";             
@@ -9,6 +10,7 @@ import userRouter from "./routes/userRouter.js";
 
 
 const app=express();             
+dotenv.config();    // enviramental variable           
 
 
 
@@ -24,11 +26,11 @@ app.use('/products',productRouter);
 app.use('/user',userRouter);         
 
 
-const CONNECTION_URL = 'mongodb+srv://Crud_App_DB:scO70ZvHjQm0jVU8@cluster0.rdppn.mongodb.net/DB2?retryWrites=true&w=majority';
-const PORT = process.env.PORT|| 5000;              
+// const CONNECTION_URL = 'mongodb+srv://Crud_App_DB:scO70ZvHjQm0jVU8@cluster0.rdppn.mongodb.net/DB2?retryWrites=true&w=majority';
+// const PORT = process.env.PORT|| 5000;              
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(PORT, () => console.log(`SERVER RUNING on Port: http://localhost:${PORT}`)))  
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(process.env.PORT, () => console.log(`SERVER RUNING on Port: http://localhost:${process.env.PORT}`)))  
   .catch((error) => console.log(`${error} DID  NOT connect`));        
 
 
